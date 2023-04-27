9e701c92f2790711b4fbe7770a550989a75293de
lyh030725 <lyh030725@gmail.com>
2023-04-27 15:59:48 +0900

    4/27

--- MyChatBot/MyChatBot.py
+++ MyChatBot/MyChatBot.py
@@ -1,13 +1,12 @@
-#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
-
 import openai
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 messages = []
 while True:
   content = input("User: ")
   messages.append({"role":"user", "content":content})
+  # messages.append({"role":"system", "content":"너는 어린 아이들을 위한 학습용 챗봇이야. 아이들이 학습 이외의 말을 하면 학습용 프로그램임을 알려줘"})
   completion = openai.ChatCompletion.create(
     model = "gpt-3.5-turbo",
     messages = messages
